8db8af80937f3eb16e1cf51723cf5bcb9345661c
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-02-12 14:54:50 +0530

    Create start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,4 +1,3 @@
-
 from pyrogram import Client
 from pyrogram.types import *
 from pyrogram import filters
@@ -38,7 +37,7 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
 
         else:
             username = datas[-2]
-            msg  = Client.get_messages(username, message_ids=msgid)
+            msg  = Client.get_messages(chat_id=username, msgid)
     
             if "Document" in str(msg):
                 Client.send_document(message.chat.id, msg.document.file_id, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)
