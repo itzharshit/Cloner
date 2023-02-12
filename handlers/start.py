92c1a499e7918c7b95cae3c3d2413c46a9564f29
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2023-02-12 12:58:46 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -80,74 +80,7 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
         if "https://t.me/c/" in message.text:
             #chatid = int("-100" + datas[-2])
             Client.send_message(message.chat.id, "private links not supported.", entities=msg.entities, reply_to_message_id=message.id)
-"""
-            with acc:
-                msg  = acc.get_messages(chatid,msgid)
 
-                if "text" in str(msg):
-                    Client.send_message(message.chat.id, msg.text, entities=msg.entities, reply_to_message_id=message.id)
-                    return
-
-                smsg = Client.send_message(message.chat.id, '__Downloading__', reply_to_message_id=message.id)
-                dosta = threading.Thread(target=lambda:downstatus(f'{message.id}downstatus.txt',smsg),daemon=True)
-                dosta.start()
-                file = acc.download_media(msg, progress=progress, progress_args=[message,"down"])
-                os.remove(f'{message.id}downstatus.txt')
-
-                upsta = threading.Thread(target=lambda:upstatus(f'{message.id}upstatus.txt',smsg),daemon=True)
-                upsta.start()
-
-            if "Document" in str(msg):
-                try:
-                    with acc:
-                        thumb = acc.download_media(msg.document.thumbs[0].file_id)
-                except:
-                    thumb = None
-                Client.send_document(message.chat.id, file, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-                if thumb != None:
-                    os.remove(thumb)
-
-            elif "Video" in str(msg):
-                try:
-                    with acc:
-                        thumb = acc.download_media(msg.video.thumbs[0].file_id)
-                except:
-                    thumb = None
-                Client.send_video(message.chat.id, file, duration=msg.video.duration, width=msg.video.width, height=msg.video.height, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-                if thumb != None:
-                    os.remove(thumb)
-
-            elif "Animation" in str(msg):
-                Client.send_animation(message.chat.id, file, reply_to_message_id=message.id)
-               
-            elif "Sticker" in str(msg):
-                Client.send_sticker(message.chat.id, file, reply_to_message_id=message.id)
-
-            elif "Voice" in str(msg):
-                Client.send_voice(message.chat.id, file, caption=msg.caption, thumb=thumb, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])
-
-            elif "Audio" in str(msg):
-                try:
-                    with acc:
-                        thumb = acc.download_media(msg.audio.thumbs[0].file_id)
-                except:
-                    thumb = None
-                Client.send_audio(message.chat.id, file, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id, progress=progress, progress_args=[message,"up"])   
-                if thumb != None:
-                    os.remove(thumb)
-
-            elif "Photo" in str(msg):
-                Client.send_photo(message.chat.id, file, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=message.id)
-
-
-            os.remove(file)
-            if os.path.exists(f'{message.id}upstatus.txt'):
-                os.remove(f'{message.id}upstatus.txt')
-            Client.delete_messages(message.chat.id,[smsg.id])
-                
-        
-"""
-        # public
         else:
             username = datas[-2]
             msg  = Client.get_messages(username,msgid)
